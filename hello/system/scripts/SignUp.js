f7462bb1ad33e0cbd98e260804132699c7379895
Athena <43275423+simmingcat@users.noreply.github.com>
2019-10-30 13:49:44 -0600

    lo

--- hello/system/scripts/SignUp.js
+++ hello/system/scripts/SignUp.js
@@ -1,12 +1,30 @@
-function Username() {
-  localStorage.removeItem('name');
-  var myName = localStorage.getItem('name');
-  myName
+function Email() {
+  if(name)
+  name==Username
+  return Username
 }
 
-function back(){
-  document.getElementById("buttons").onclick = function () {
-      home.href = "file:///C:/Users/StarFoxPlays/Documents/GitHub/gaming-website/hello/icon/mod.html";
-   }
+function main() {
+  //console.log("It works");
+
+  let name;
+
+  //Get the Width-value from id=textfield1
+   = document.getElementById('name').value;
+  //testNaN (firstNumber);
+  document.getElementById('name').innerHTML = alert ( testNaN (name));
+  document.getElementById('name').innerHTML = testNaN (name);
+
+
+   = document.getElementById('').value;
+
+  document.getElementById('').innerHTML = alert ( testNaN ());
+  document.getElementById('').innerHTML = testNaN ();
+
+  //Get the Height-value from id=textfield2
+   = document.getElementById('').value;
+  //testNaN (secondNumber);
+  document.getElementById('').innerHTML = alert ( testNaN ());
+  document.getElementById('').innerHTML = testNaN ();
 
 }
--- hello/system/scripts/login.js
+++ hello/system/scripts/login.js
@@ -1,18 +1,5 @@
-function validateForm() {
-  var x = document.forms[""][""].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
-
 function Sub() {
-  var modal = document.getElementById('id01');
+  if (sub) {
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
   }
-}
 }
